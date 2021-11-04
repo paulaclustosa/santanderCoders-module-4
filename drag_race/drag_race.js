387ee9d73906b669051279ef20248d5b51f28f32
Paula Lustosa <86850247+paulaclustosa@users.noreply.github.com>
2021-11-04 16:35:46 -0300

    Update drag_race.js

--- drag_race/drag_race.js
+++ drag_race/drag_race.js
@@ -1,118 +1,112 @@
+const input = document.querySelector("input");
+const form = document.querySelector("form");
+const stopwatch = document.querySelector(".stopwatch");
+
+const _STOPWATCH_OPEN_CLASS = "--start-stopwatch";
+
 let timeId = undefined;
-
-const input = document.querySelector('input')
-const form = document.querySelector('form')
-const stopwatch = document.querySelector('.stopwatch')
-
-const _STOPWATCH_OPEN_CLASS = "--start-stopwatch"
 
 const runStopwatch = () => {
   if (!input.checkValidity()) {
-    return
-  }
+    return;
+  } else {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
 
-  else {
-    form.addEventListener('submit', (event) => {
-      event.preventDefault()
-    })
-
-    stopwatch.classList.add(_STOPWATCH_OPEN_CLASS)
+    stopwatch.classList.add(_STOPWATCH_OPEN_CLASS);
 
     if (timeId === undefined) {
-      timeId = setInterval(updateTime, 1000)
+      timeId = setInterval(updateTime, 1000);
     }
   }
-}
+};
 
 let minutes = 0;
 let seconds = 0;
 
-const time = document.querySelector('span[name="time"]')
+const time = document.querySelector("span");
 
 const updateTime = () => {
-  seconds += 1
+  seconds += 1;
 
   if (seconds === 60) {
-    minutes += 1
-    seconds = 0
+    minutes += 1;
+    seconds = 0;
   }
 
-  displayTime(time)
-}
+  displayTime(time);
+};
 
 const displayTime = (element) => {
-  const min = minutes < 10 ? "0" + minutes : minutes
-  const sec = seconds < 10 ? "0" + seconds : seconds
-  element.innerText = `${min}:${sec}`
-}
+  const min = minutes < 10 ? "0" + minutes : minutes;
+  const sec = seconds < 10 ? "0" + seconds : seconds;
+  element.innerText = `${min}:${sec}`;
+};
 
-const list = document.querySelector('ul')
+const list = document.querySelector("ul");
 
 const stopStopwatch = () => {
+  let stopwatchClassListArray = [...stopwatch.classList];
 
-  let stopwatchClassListArray = [...stopwatch.classList]
+  if (!stopwatchClassListArray.includes("--start-stopwatch")) {
+    return;
+  } else {
+    stopwatch.classList.remove(_STOPWATCH_OPEN_CLASS);
 
-  if (!stopwatchClassListArray.includes('--start-stopwatch')) {
-    return
+    clearInterval(timeId);
+    timeId = undefined;
+
+    createListItem(setPlayerData());
+    addPlayerToSessionStorage(setPlayerData());
+    addBestPlayerToLocalStorage();
+
+    input.value = "";
+    minutes = 0;
+    seconds = 0;
+    displayTime(time);
   }
-
-  else {
-    stopwatch.classList.remove(_STOPWATCH_OPEN_CLASS)
-
-    clearInterval(timeId)
-    timeId = undefined
-
-    createListItem(setPlayerData())
-    addPlayerToSessionStorage(setPlayerData())
-    addBestPlayerToLocalStorage()
-
-    input.value = ''
-    minutes = 0
-    seconds = 0
-    displayTime(time)
-  }
-}
+};
 
 const createListItem = (playerData) => {
-  const listItem = document.createElement('li')
-  list.appendChild(listItem)
+  const listItem = document.createElement("li");
+  list.appendChild(listItem);
 
-  listItem.innerText = `${playerData.name} | ${playerData.time} seconds`
-}
+  listItem.innerText = `${playerData.name} | ${playerData.time} seconds`;
+};
 
 const setPlayerData = () => {
-  const playerName = input.value
-  const playerTimeInSeconds = minutes * 60 + seconds
-  const playerData = { name: playerName, time: playerTimeInSeconds }
-  return playerData
-}
+  const playerName = input.value;
+  const playerTimeInSeconds = minutes * 60 + seconds;
+  const playerData = { name: playerName, time: playerTimeInSeconds };
+  return playerData;
+};
 
 let playerKey = 0;
 
 const addPlayerToSessionStorage = (playerData) => {
-  playerKey += 1
-  sessionStorage.setItem(playerKey, JSON.stringify(playerData))
-}
+  playerKey += 1;
+  sessionStorage.setItem(playerKey, JSON.stringify(playerData));
+};
 
 const addBestPlayerToLocalStorage = () => {
-  const players = Object.values(sessionStorage)
-    .map(player => JSON.parse(player))
+  const players = Object.values(sessionStorage).map((player) =>
+    JSON.parse(player)
+  );
 
-  const playersTime = players
-    .map(player => player.time)
+  const playersTime = players.map((player) => player.time);
 
-  const minTime = Math.min(...playersTime)
+  const minTime = Math.min(...playersTime);
 
-  const bestPlayer = players
-    .filter(player => player.time === minTime)
+  const bestPlayer = players.filter((player) => player.time === minTime);
 
-  localStorage.setItem("best player", JSON.stringify(bestPlayer))
-}
+  localStorage.setItem("best player", JSON.stringify(bestPlayer));
+};
 
-const startButton = document.querySelector('button[name="start"]')
+const startButton = document.querySelector('button[name="start"]');
 
-startButton.addEventListener('click', runStopwatch)
+startButton.addEventListener("click", runStopwatch);
 
-const stopButton = document.querySelector('button[name="stop"]')
+const stopButton = document.querySelector('button[name="stop"]');
 
-stopButton.addEventListener('click', stopStopwatch)
+stopButton.addEventListener("click", stopStopwatch);
